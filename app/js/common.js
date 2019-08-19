e48929ab0baa09245ba0c0b9b3c8778619f7059c
USER-PC\Tom <jedi.wasya@gmail.com>
2019-08-19 04:27:25 +0300

    second commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,34 +1,45 @@
 jQuery(document).ready(function($) {
     $("#fullpage").fullpage({
         // sectionsColor:['#C63D0F','#1BBC9B','#7E8F7C'],
-        anchors:['firstPage','secondPage','3rdPage'],
+        anchors:['firstPage','secondPage','3rdPage','4rdPage','5rdPage'],
         menu:'#menu',
         scrollBar:false,
         //scrollOverflow:true
         navigation:true,
-        navigationTooltips:['Секция 1','Секция 2','Секция 3'],
-        slidesNavigation:true,
+        navigationTooltips:['Секция 1','Секция 2','Секция 3','Секция 4','Секция 5'],
+        slidesNavigation:false,
         navigationPosition:'left',
         //slidesNavPosition:'top'
-        loopTop:true,
-        loopBottom:true,
-        loopHorizontal:false,
+        // loopTop:false,
+        // loopBottom:false,
+        // loopHorizontal:false,
 
         // afterResize:function(link,index) {
         //     alert('Hello');
         // },
-        afterLoad:function(link,index) {
-
-            if(index == 3) {
-                $("#section2 img").delay(2000).animate({'left':'0%'},2000);
-            }
-            if(link == '3rdPage') {
-                $("#section2 h1").fadeIn(1500,function() {
-                    $("#section2 p").css({'display':'block'}).animate({'fontSize':'8em'},1000)
-                });
-            }
-
+        // afterLoad:function(origin) {
+        //
+        //     if(origin) {
+        //         $("#section0 img").delay(300).animate({'left':'0%'},300);
+        //         $("#section0 h1").fadeIn(300,function() {
+        //                    $("#section0 p").css({'display':'block'}).animate({'fontSize':'8em'},300)
+        //                  });
+        //     }
+        //     if(link == 'firstPage') {
+        //         $("#section0 h1").fadeIn(300,function() {
+        //             $("#section0 p").css({'display':'block'}).animate({'fontSize':'8em'},300)
+        //         });
+        //     }
+        //
+        // },
+        afterRender: function(){
+            var pluginContainer = this;
+            $("#section0 img").delay(300).fadeIn(1300);
+            $("#section0 h1").fadeIn(1500,function() {
+                $("#section0 p").css({'display':'block'}).animate({'fontSize':'8em'},300)
+            });
         }
+
 
     });
 })
